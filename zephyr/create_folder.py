504b2954cd847ba43698a44a57eb27cc6141b155
JanLackVay <jan.lack@vay.io>
2024-01-24 13:16:42 +0100

    Clean and modulize code

--- zephyr/create_folder.py
+++ zephyr/create_folder.py
@@ -1,29 +1,35 @@
 import argparse
+import time
 from typing import List, Optional
 
 import requests
 import yaml
 
 URL = "https://api.zephyrscale.smartbear.com/v2"
+PROJECTKEY = "REE"
+TEST_EXECTION_STATUS = "Not Executed"
 FOLDER_ID_VSR_TEST_CYCLE = 9263028
 FOLDER_ID_VDRIVE_TEST_CYCLE = 9141542
 
 
-def main(vsr_version: str, vdrive_version: str, vreecu_version):
+def main(vsr_version: str, vdrive_version: str, vreecu_version, regression: bool):
     api_token = yaml.safe_load(open("credentials.yaml"))["api_token"]
     headers_api = {
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Authorization": api_token,
     }
-    test_case_information = get_all_test_cases(
-        url=f"{URL}/testcases?", headers_api=headers_api
-    )
+
+    test_case_url = get_zephyr_url(URL, "testcases")
+    test_execution_url = get_zephyr_url(URL, "testexecutions")
+    test_folder_url = get_zephyr_url(URL, "folders")
+
+    test_case_information = get_all_test_cases(test_case_url, headers_api)
 
     if not vsr_version:
         print("Create vDrive test cycle")
         test_cycle_information = create_vdrive_test_cyle(
-            headers_api, vdrive_version, test_case_information
+            headers_api, vdrive_version, test_case_information, test_execution_url
         )
     else:
         print("Create VSR test cycle")
@@ -33,8 +39,47 @@ def main(vsr_version: str, vdrive_version: str, vreecu_version):
             vdrive_version,
             vreecu_version,
             test_case_information,
+            test_execution_url,
+            test_folder_url,
+            regression,
         )
     return test_cycle_information
+
+
+def get_zephyr_url(url: str, application: str):
+    return f"{url}/{application}"
+
+
+def make_request(url, method="GET", headers=None, json_data=None, params=None):
+    try:
+        if method == "GET":
+            response = requests.get(url, headers=headers, params=params)
+        elif method == "POST":
+            response = requests.post(url, headers=headers, json=json_data)
+
+        response.raise_for_status()
+
+        if response.status_code == 403:
+            handle_rate_limit(response)
+
+        return response.json()
+    except requests.exceptions.RequestException as err:
+        print(f"Request error occurred: {err}")
+
+
+def handle_rate_limit(response):
+    remaining_requests = int(response.headers.get("X-RateLimit-Remaining", 0))
+    reset_time = int(response.headers.get("X-RateLimit-Reset", 0))
+
+    if remaining_requests == 0:
+        # Sleep until the rate limit resets
+        sleep_time = max(0, reset_time - int(time.time()))
+        print(f"Rate limit exceeded. Sleeping for {sleep_time} seconds.")
+        time.sleep(sleep_time)
+        # Make the request again
+        return True
+    else:
+        return False
 
 
 def create_vsr_test_cycle(
@@ -43,136 +88,65 @@ def create_vsr_test_cycle(
     vdrive_version: str,
     vreecu_version: str,
     test_case_information: dict[str, str],
+    test_execution_url: str,
+    test_folder_url: str,
+    regression: bool,
 ):
     parent_id = create_vsr_folder(
         headers_api,
-        url=f"{URL}/folders",
+        url=test_folder_url,
         name=f"VSR {vsr_version}",
         parent_id=FOLDER_ID_VSR_TEST_CYCLE,
     )
-    # convetional
-    conventional_cycle_key = create_test_cycle_folder(
-        headers_api,
-        parent_id,
-        name=f"VSR {vsr_version} Conventional [vDrive {vdrive_version}, vREECU {vreecu_version}]",
-    )
-    conventional_tests = filter_test_cases(
-        test_case_information, desired_labels=["Conventional"]
-    )
-    assign_testcases_to_testcycle(
-        url=f"{URL}/testexecutions",
-        headers_api=headers_api,
-        testCycleKey=conventional_cycle_key,
-        test_cases=conventional_tests,
-    )
 
-    # core
-    core_cycle_key = create_test_cycle_folder(
-        headers_api,
-        parent_id,
-        name=f"VSR {vsr_version} Core Cycle [vDrive {vdrive_version}, vREECU {vreecu_version}]",
-    )
-    core_tests = filter_test_cases(
-        test_case_information,
-        desired_labels=["vDrive_sys", "vREECU_sys"],
-        w_sd_config=True,
-        wo_sd_config=True,
-    )
-    assign_testcases_to_testcycle(
-        url=f"{URL}/testexecutions",
-        headers_api=headers_api,
-        testCycleKey=core_cycle_key,
-        test_cases=core_tests,
-    )
+    cycle_types = [
+        "conventional",
+        "core",
+        "with_sd",
+        "without_sd",
+        "minimal_us",
+        "conventional_us",
+    ]
 
-    # without sd
-    wo_sd_cycle_key = create_test_cycle_folder(
-        headers_api,
-        parent_id,
-        name=f"VSR {vsr_version} w/o_SD Cycle [vDrive {vdrive_version}, vREECU {vreecu_version}]",
-    )
+    if regression:
+        print("Create regression test cycle")
+        cycle_types.append("regression")
 
-    wo_sd_tests = filter_test_cases(
-        test_case_information,
-        desired_labels=["vDrive_sys", "vREECU_sys"],
-        w_sd_config=False,
-        wo_sd_config=True,
-    )
-    assign_testcases_to_testcycle(
-        url=f"{URL}/testexecutions",
-        headers_api=headers_api,
-        testCycleKey=wo_sd_cycle_key,
-        test_cases=wo_sd_tests,
-    )
+    cycle_information = {}
 
-    # with sd
-    w_sd_cycle_key = create_test_cycle_folder(
-        headers_api,
-        parent_id,
-        name=f"VSR {vsr_version} w_SD Cycle [vDrive {vdrive_version}, vREECU {vreecu_version}]",
-    )
+    for cycle_type in cycle_types:
+        cycle_information[cycle_type] = {
+            "name": f"VSR {vsr_version} {cycle_type.capitalize()} Cycle [vDrive {vdrive_version}, vREECU {vreecu_version}]",
+            "labels": ["Conventional"]
+            if cycle_type == "conventional_us"
+            else ["vDrive_sys", "vREECU_sys"],
+        }
 
-    w_sd_tests = filter_test_cases(
-        test_case_information,
-        desired_labels=["vDrive_sys", "vREECU_sys"],
-        w_sd_config=True,
-        wo_sd_config=False,
-    )
-    assign_testcases_to_testcycle(
-        url=f"{URL}/testexecutions",
-        headers_api=headers_api,
-        testCycleKey=w_sd_cycle_key,
-        test_cases=w_sd_tests,
-    )
+        cycle_information[cycle_type]["key"] = create_test_cycle_folder(
+            headers_api,
+            parent_id,
+            name=cycle_information[cycle_type]["name"],
+        )
 
-    # us minimal
-    minimal_us_cycle_key = create_test_cycle_folder(
-        headers_api,
-        parent_id,
-        name=f"VSR {vsr_version} US Minimal [vDrive {vdrive_version}, vREECU {vreecu_version}]",
-    )
+        cycle_information[cycle_type]["test_cases"] = filter_test_cases(
+            test_case_information,
+            desired_labels=cycle_information[cycle_type]["labels"],
+        )
+        assign_testcases_to_testcycle(
+            url=test_execution_url,
+            headers_api=headers_api,
+            testCycleKey=cycle_information[cycle_type]["key"],
+            test_cases=cycle_information[cycle_type]["test_cases"],
+        )
 
-    us_min_tests = filter_test_cases(
-        test_case_information,
-        desired_labels=["US_release_O3.3"],
-    )
-    assign_testcases_to_testcycle(
-        url=f"{URL}/testexecutions",
-        headers_api=headers_api,
-        testCycleKey=minimal_us_cycle_key,
-        test_cases=us_min_tests,
-    )
-
-    # us conventional
-    conventional_us_cycle_key = create_test_cycle_folder(
-        headers_api,
-        parent_id,
-        name=f"VSR {vsr_version} Conventional US [vDrive {vdrive_version}, vREECU {vreecu_version}]",
-    )
-    us_conv_tests = filter_test_cases(
-        test_case_information, desired_labels=["Conventional"]
-    )
-    assign_testcases_to_testcycle(
-        url=f"{URL}/testexecutions",
-        headers_api=headers_api,
-        testCycleKey=conventional_us_cycle_key,
-        test_cases=us_conv_tests,
-    )
-
-    return {
-        "conventional_cycle_key": conventional_cycle_key,
-        "core_cycle_key": core_cycle_key,
-        "wo_sd_cycle_key": wo_sd_cycle_key,
-        "w_sd_cycle_key": w_sd_cycle_key,
-        "minimal_us_cycle_key": minimal_us_cycle_key,
-        "conventional_us_cycle_key": conventional_us_cycle_key,
-    }
+    return set(cycle_information[cycle_type]["key"] for cycle_type in cycle_types)
 
 
 def create_vdrive_test_cyle(
     headers_api: dict[str, str],
     vdrive_version: str,
     test_case_information: dict[str, str],
+    test_execution_url: str,
 ):
     vdrive_tests = filter_test_cases(test_case_information, desired_labels=["vDrive"])
     print(vdrive_tests)
@@ -184,7 +158,7 @@ def create_vdrive_test_cyle(
     )
 
     assign_testcases_to_testcycle(
-        url=f"{URL}/testexecutions",
+        url=test_execution_url,
         headers_api=headers_api,
         testCycleKey=vdrive_cycle_key,
         test_cases=vdrive_tests,
@@ -194,13 +168,13 @@ def create_vdrive_test_cyle(
 
 def get_all_test_cases(url: str, headers_api: dict[str, str]):
     parameters = {"maxResults": 1}
-    initial_test_case_information = requests.get(
-        url, headers=headers_api, params=parameters
-    ).json()
+    initial_test_case_information = make_request(
+        url, method="GET", headers=headers_api, params=parameters
+    )
     number_of_test_cases = initial_test_case_information["total"]
 
     parameters = {"maxResults": number_of_test_cases}
-    return requests.get(url, headers=headers_api, params=parameters).json()
+    return make_request(url, method="GET", headers=headers_api, params=parameters)
 
 
 def create_vsr_folder(headers_api: dict[str, str], url: str, name: str, parent_id: int):
@@ -210,9 +184,8 @@ def create_vsr_folder(headers_api: dict[str, str], url: str, name: str, parent_i
         "projectKey": "REE",
         "folderType": "TEST_CYCLE",
     }
-    headers = headers_api
-    response = requests.post(url, json=payload, headers=headers)
-    return response.json()["id"]
+    response = make_request(url, method="POST", headers=headers_api, json_data=payload)
+    return response["id"]
 
 
 def create_test_cycle_folder(headers_api: dict[str, str], parent_id: int, name: str):
@@ -223,9 +196,9 @@ def create_test_cycle_folder(headers_api: dict[str, str], parent_id: int, name: 
         "name": name,
         "projectKey": "REE",
     }
-    response = requests.post(url, json=payload, headers=headers_api)
-    print(name + " " + response.text)
-    return response.json()["key"]
+    response = make_request(url, method="POST", headers=headers_api, json_data=payload)
+    print(name, response)
+    return response["key"]
 
 
 def filter_test_cases(
@@ -253,18 +226,15 @@ def filter_test_cases(
 
 
 def assign_testcases_to_testcycle(url, headers_api, testCycleKey, test_cases):
-    url = "https://api.zephyrscale.smartbear.com/v2/testexecutions"
-    parameters = {
-        "projectKey": "REE",
+    payload = {
+        "projectKey": PROJECTKEY,
         "testCycleKey": testCycleKey,
-        "statusName": "Not Executed",
+        "statusName": TEST_EXECTION_STATUS,
     }
 
     for test_case in test_cases:
-        parameters["testCaseKey"] = test_case
-        initial_test_case_information = requests.post(
-            url, headers=headers_api, json=parameters
-        ).json()
+        payload["testCaseKey"] = test_case
+        make_request(url, method="POST", headers=headers_api, json_data=payload)
 
 
 def parse_args():
@@ -284,6 +254,12 @@ def parse_args():
         "--vreecu-version",
         type=str,
         help="vREECU version. <major>.<minor>.<patch>",
+    )
+    parser.add_argument(
+        "--regression",
+        type=lambda x: x.lower() == "true",
+        required=True,
+        help="If regression test cycle should be created true, else false.",
     )
     return parser.parse_args()
 
